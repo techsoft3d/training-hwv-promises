0eedb09225de451878b1b3cc94d3876d2fab2e5a
Adam DuQuette <adam.duquette@techsoft3d.com>
2021-03-09 09:13:53 -0800

    add completed version of training

--- demo_hwv.js
+++ demo_hwv.js
@@ -1,34 +1,112 @@
 'use strict';
 
+function createScsViewer(scsModelName, containerId) {
+  return new Promise(function (resolve, reject) {
+    var viewer = new Communicator.WebViewer({
+      containerId: containerId,
+      endpointUri: `data/${scsModelName}.scs`
+    });
+
+    resolve(viewer);
+  });
+};
+
+function registerCallbacks(viewer) {
+    let callbacks = {
+      sceneReady: () => {
+        viewer.view.setBackgroundColor(Communicator.Color.blue(), Communicator.Color.white());
+      },
+      modelStructureReady: () => {
+        console.log(`Model structure is ready!`);
+      },
+      camera: () => {
+        console.log(`Camera change detected:`);
+        console.table(JSON.stringify(viewer.view.getCamera().toJson(), null, 4));
+      },
+    };
+
+    viewer.setCallbacks(callbacks);
+    return viewer;
+}
+
 var hwv;
 
 window.onload = () => {
   let scsModelName = "landinggear";
+  let containerId = "viewer";
 
-  hwv = new Communicator.WebViewer({
-      containerId: "viewer",
-      endpointUri: `data/${scsModelName}.scs`
-  });
-
-  hwv.start();
+  createScsViewer(scsModelName, containerId)
+    .then(registerCallbacks)
+    .then(viewer => {
+      hwv = viewer;
+      hwv.start()
+    });
 }
 
 // Local procedure to wrap model.loadSubtreeFromScsFile logic
-// source: https://docs.techsoft3d.com/communicator/latest/build/api_ref/typedoc/classes/communicator.model.html#loadsubtreefromscsfile
+var loadModelAsSubtree = (modelName) => {
+  var model = hwv.model;
+  
+  // Retrieve root node of model
+  var rootNode = model.getAbsoluteRootNode();
+  
+  // Create a child node of root node
+  var id = model.createNode(rootNode, modelName);
+  
+  // Load provided model to newly created node
+  // Remember to return the promise! See myWaitFunction() in demo_promises.js for reference
+  return model.loadSubtreeFromScsFile(id, `data/${modelName}.scs`);
+
+  // If load is successful, loadSubtreeFromScsFile will produce the newly loaded model's root nodes IDs.
+  // source: https://docs.techsoft3d.com/communicator/latest/build/api_ref/typedoc/classes/communicator.model.html#loadsubtreefromscsfile
+};
 
 // Demonstrate Promises + async/await with HOOPS Web Viewer
-// source: https://docs.techsoft3d.com/communicator/latest/build/api_ref/typedoc/classes/communicator.model.html#getnodeproperties
+let printFirstChildProperties = async nodeIds => {
+  let children = hwv.model.getNodeChildren(nodeIds[0])
+  let props = await hwv.model.getNodeProperties(children[0])
+  console.log(props);
+
+  // source: https://docs.techsoft3d.com/communicator/latest/build/api_ref/typedoc/classes/communicator.model.html#getnodeproperties
+};
 
 // Demonstration of using Promises to load multiple models using .then() chaining
 let handleOnClickThenChain = () => {
 
-  hwv.model.clear(); // Clear the model
+  hwv.model.clear() // Clear the model
+    
+    .then(() => loadModelAsSubtree('landinggear')) // load first model
+    .then((nodeId) => console.log(`nodeId: ${nodeId}`)) // arbitrary unit of work
+
+    .then(() => loadModelAsSubtree('microengine')) // load second
+    .then(printFirstChildProperties) // return value is automatically passed to function
+  
+    .then(() => loadModelAsSubtree('MountainHome')) // load third
+    .then((nodeId) => console.log(`nodeId: ${nodeId}`)) // arbitrary unit of work
+
+    .catch(console.error) // handle any errors from above
+    .finally(() => console.log('Loading promise chain complete!'));
 };
 
 // Demonstration of using Promises to load multiple models using try/catch asyc/await
 let handleOnClickAsyncAwait = async () => {
 
-  hwv.model.clear(); // Clear the model
+  try {
+    await hwv.model.clear(); // Clear the model
+
+    var nodeId = await loadModelAsSubtree('landinggear'); // load first model
+    console.log(`nodeId: ${nodeId}`);
+
+    nodeId = await loadModelAsSubtree('microengine'); // load second
+    printFirstChildProperties(nodeId);
+
+    nodeId = await loadModelAsSubtree('MountainHome'); // load third
+    console.log(`nodeId: ${nodeId}`);
+  } catch(err) {
+    console.error(err);
+  }
+  
+  console.log('Loading promise chain complete!'); // finally
 };
 
 // Attach onClick event handlers to DOM
--- demo_promises.js
+++ demo_promises.js
@@ -20,21 +20,59 @@ Promise states:
 
 // First, we wrap our async task in an executor function: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/Promise
 var executor = function(resolve, reject){
+  // Prepare data
+  let timer = Math.floor(Math.random() * 1000);
+
+  // Send async request
+  setTimeout(() => {
+    if (timer % 2 === 0) {
+      resolve(`Timer is even! ${timer}`); // Async task successful!
+    }else {
+      reject(`Timer is odd! ${timer}`); // Async task Unsuccessful : (
+    }
+  }, timer);
 };
 
 // Now that our async task is wrapped in an executor function, we create the Promise *object*
+var asyncTaskAsAPromiseObject = new Promise(executor);
 
 // At this point, the status is *pending* and the value is *undefined*
 debugger;
 
 //let's chain our logic together using .then()
+asyncTaskAsAPromiseObject 
+  .then(value => { debugger; console.log(`Results: ${value}`)}) // Handle when 'promise' status is *resolved*
+  .then(() => console.log('wow this is great')) // arbitrary unit of work 
+  .catch(errorMessage => { debugger; console.error(errorMessage);}) //Handle when 'promise' status from above is *rejected*
+  .finally(() => console.log('performing post-task clean up...')); // alway run at end, regardless of success or failure from above
 
 
 // Let's wrap the asyncTaskAsAPromiseObject in a function so we can pass arguments to the executor
 var myWaitFunction = (timer) => {
+  // perform some work...
+
+  // We return a Promise object so we can use .then() chaining
+  return new Promise((resolve, reject) => {
+    // Send async request
+    setTimeout(() => {
+      if (timer % 2 === 0) {
+        resolve(`Timer is even! ${timer}`); // Async task successful!
+      }else {
+        reject(`Timer is odd! ${timer}`); // Async task UNsuccessful : (
+      }
+    }, timer);
+  });
 }
 
 // Now we can call as a function
+myWaitFunction(1000)
+  .then(value => { debugger; console.log(`Results: ${value}`)}) // Handle when 'promise' status is *resolved*
+  .then(() => console.log('wow this is great')) // arbitrary unit of work 
+  .catch(errorMessage => { debugger; console.error(errorMessage);}) //Handle when 'promise' status from above is *rejected*
+  .finally(() => console.log('performing post-task clean up...')); // alway run at end, regardless of success or failure from above
   
 // Promise.all() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 // Promise.race() - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/race
+var myListOfPromises = [myWaitFunction(1000), myWaitFunction(2000)];
+Promise.all(myListOfPromises)
+  .then((result) => console.log(result[1])); // result is array[] of each resolve/rejected Promise
